93e89e86d5537cc790e6026baf18dddc865f06b8
SolidityDrone <pnn.claudio@gmail.com>
2023-11-09 18:43:43 +0100

    This code has to be refactored. To run test run ' forge test --fork-url polygonRpcUrl -vvv '

--- Solidity/src/ArbitrageExecuter.sol
+++ Solidity/src/ArbitrageExecuter.sol
@@ -6,16 +6,21 @@ import "@balancerV2/interfaces/contracts/vault/IFlashLoanRecipient.sol";
 
 pragma solidity ^0.8.6;
 
+// Naive demonstration with DAI token.
+// The transaction may revert with err BAL#528 
+//      if the Vault dosent hold enough tokens
 
-contract FlashLoanRecipient is IFlashLoanRecipient {
+contract ArbitrageExecuter is IFlashLoanRecipient {
+
+    
     IVault private _vault;
-    address public USDC = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
+    address public DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
     uint256 public amountLoaned;
     constructor(){
         _vault = IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
     }
     
-
+    // This function request loan on vault 
     function makeFlashLoan(
         IERC20[] memory tokens,
         uint256[] memory amounts,
@@ -24,6 +29,7 @@ contract FlashLoanRecipient is IFlashLoanRecipient {
       _vault.flashLoan(IFlashLoanRecipient(this), tokens, amounts, userData);
     }
 
+    // Vault calls this function and expect the loan to be repaid in the same tx
     function receiveFlashLoan(
         IERC20[] memory tokens,
         uint256[] memory amounts,
@@ -31,7 +37,7 @@ contract FlashLoanRecipient is IFlashLoanRecipient {
         bytes memory userData
     ) external override {
         require(msg.sender == address(_vault));
-        amountLoaned = IERC20(USDC).balanceOf(address(this));
-        IERC20(USDC).transfer(address(_vault), IERC20(USDC).balanceOf(address(this)));
+        amountLoaned = IERC20(DAI).balanceOf(address(this));
+        IERC20(DAI).transfer(address(_vault), IERC20(DAI).balanceOf(address(this)));
     }
 }
--- Solidity/test/test.t.sol
+++ Solidity/test/test.t.sol
@@ -3,18 +3,18 @@
 pragma solidity ^0.8.6;
 
 import "std/test.sol";
-import "../src/contract.sol";
+import "../src/ArbitrageExecuter.sol";
 
-contract Test_Sample is Test {
-    IERC20 public USDC;
-    FlashLoanRecipient public flr;
+contract Test_ArbitrageExecuter is Test {
+    IERC20 public DAI;
+    ArbitrageExecuter public flr;
 
     IERC20[] public erc20List;
     uint256[] public amounts;
     function setUp() public {
-        flr = new FlashLoanRecipient();
-        USDC = IERC20(address(0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063));
-        erc20List.push(USDC);
+        flr = new ArbitrageExecuter();
+        DAI = IERC20(address(0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063));
+        erc20List.push(DAI);
         amounts.push(1e22);
     }
     
